9995d877d14c4f3fa875c4ad58152da6c14ea87a
angelSantori <antonyo.da.mm@gmail.com>
2023-10-26 09:43:36 -0700

    Directivas *ngIf y *ngFor

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app1';
+
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  sueldos = [1700, 1600, 1900];
 }
+
+
